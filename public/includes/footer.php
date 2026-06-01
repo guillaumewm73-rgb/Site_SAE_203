45e828065293b7ab7a8cc294707c88bb04791078
guillaume_Moulin <guillaumewm73@gmail.com>
2026-06-01 13:11:35 +0200

    Intégration logo footer

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -4,10 +4,16 @@
             <p>Exposition d’œuvres multimédia interactives des étudiant·es MMI.</p>
             <p>28 Av du Lac d’Annecy, 73370 Le Bourget-du-Lac</p>
         </div>
-        <div class="footer-links">
-            <a href="https://www.instagram.com/mmichambery/" target="_blank" rel="noreferrer">Instagram MMI</a>
-            <a href="https://mmi.univ-smb.fr/" target="_blank" rel="noreferrer">Site web MMI</a>
-            <a href="mailto:mmi-chambery@univ-smb.fr">mmi-chambery@univ-smb.fr</a>
+        <div class="footer-actions">
+            <div class="footer-links">
+                <a href="https://www.instagram.com/mmichambery/" target="_blank" rel="noreferrer">Instagram MMI</a>
+                <a href="https://mmi.univ-smb.fr/" target="_blank" rel="noreferrer">Site web MMI</a>
+                <a href="mailto:mmi-chambery@univ-smb.fr">mmi-chambery@univ-smb.fr</a>
+            </div>
+            <div class="footer-logos" aria-label="Logos partenaires">
+                <img class="footer-logo footer-logo-iut" src="assets/images/logo-iut.png" alt="IUT de Chambéry">
+                <img class="footer-logo footer-logo-mmi" src="assets/images/logo-mmi.png" alt="MMI Chambéry">
+            </div>
         </div>
     </footer>
 </body>
