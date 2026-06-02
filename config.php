47734a60e4ffa1f17ea36ba0fe9b7861bed756e5
Olivia R <olivia.rattin07@gmail.com>
2026-06-02 12:55:27 +0200

    essai bdd-to-site

--- config.php
+++ config.php
@@ -4,10 +4,9 @@ $estEnLocal = in_array($serveurActuel, ['localhost', '127.0.0.1', '::1'], true);
 
 if ($estEnLocal) {
     define('NOM_BD', 'sae203_bdd');
-    define('SERVEUR_BD', '127.0.0.1');
-    define('PORT_BD', '8889');
+    define('SERVEUR_BD', 'localhost');
     define('LOGIN_BD', 'root');
-    define('PASSE_BD', 'root');
+    define('PASSE_BD', '');
 } else {
     define('NOM_BD', 'ijtebowcompte13');
     define('SERVEUR_BD', 'ijtebowcompte13.mysql.db');
