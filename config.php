9f1e324cce9a714356fa158d4e86f6570407bf12
guillaume_Moulin <guillaumewm73@gmail.com>
2026-06-02 08:44:16 +0200

    b

--- config.php
+++ config.php
@@ -1,6 +1,16 @@
 <?php
-define('NOM_BD', 'ijtebowcompte13');
-define('SERVEUR_BD', 'ijtebowcompte13.mysql.db');
-define('LOGIN_BD', 'ijtebowcompte13');
-define('PASSE_BD', 'v8ng67SF2026');
+$serveurActuel = $_SERVER['SERVER_NAME'] ?? 'localhost';
+$estEnLocal = in_array($serveurActuel, ['localhost', '127.0.0.1', '::1'], true);
+
+if ($estEnLocal) {
+    define('NOM_BD', 'sae203_bdd');
+    define('SERVEUR_BD', 'localhost');
+    define('LOGIN_BD', 'root');
+    define('PASSE_BD', '');
+} else {
+    define('NOM_BD', 'ijtebowcompte13');
+    define('SERVEUR_BD', 'ijtebowcompte13.mysql.db');
+    define('LOGIN_BD', 'ijtebowcompte13');
+    define('PASSE_BD', 'v8ng67SF2026');
+}
 ?>
