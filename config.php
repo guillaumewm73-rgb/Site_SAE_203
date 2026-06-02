177f023b17f19dd7962270a4b79f2f2a4c157cef
Olivia R <olivia.rattin07@gmail.com>
2026-06-02 14:02:02 +0200

    essssssaaaaaiii

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-    define('NOM_BD', 'sae203_bdd');
-    define('SERVEUR_BD', 'localhost');
+    define('NOM_BD', 'ijtebowcompte13');
+    define('SERVEUR_BD', 'ijtebowcompte13.mysql.db');
     define('PORT_BD', 3306);
-    define('LOGIN_BD', 'root');
-    define('PASSE_BD', '');
+    define('LOGIN_BD', 'ijtebowcompte13');
+    define('PASSE_BD', 'v8ng67SF2026');
+
 ?>
