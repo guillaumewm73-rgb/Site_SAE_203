ec5dbb316693b080c45f1121a80693f6e51c1b31
Olivia R <olivia.rattin07@gmail.com>
2026-06-02 10:42:26 +0200

    Mise à jour des labels de public/includes/donnee_salles.php

--- public/includes/donnee_salles.php
+++ public/includes/donnee_salles.php
@@ -172,10 +172,10 @@ TEXT,
     ];  
 
     $roomSupportLabels = [
-        '001' => 'La Pépinière” TP 2.2 - Lotus / E-biscus / Datura / Oeuvre 4',
-        '002' => '"L\'Envers du Décors” TP 2.1 - Tapis Rouge / En Direct / AD-HD',
-        '005' => '"Horizon” TP 1.2 - Bon profil / Antithèse / Beauté hors du cadre',
-        '021' => '"Societ-e” TP 1.1 - Community / Distorsion',
+        '001' => 'TP 2.2 - Lotus / E-biscus / Datura / Oeuvre 4',
+        '002' => 'TP 2.1 - Tapis Rouge / En Direct / AD-HD',
+        '005' => 'TP 1.2 - Bon profil / Antithèse / Beauté hors du cadre',
+        '021' => 'TP 1.1 - Community / Distorsion',
     ];
 
     $catalog = [];
