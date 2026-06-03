d54107f88570f6742679d0381ab8a70f9cccb4c3
Olivia R <olivia.rattin07@gmail.com>
2026-06-03 10:51:06 +0200

    essai connexion

--- connexion.php
+++ connexion.php
@@ -1,26 +1,33 @@
 <?php
 
-define('NOM_BD', 'sae203_bdd_v1');
-define('SERVEUR_BD', '127.0.0.1');
-define('PORT_BD', 8889);
-define('LOGIN_BD', 'root');
-define('PASSE_BD', 'root');
+define('NOM_BD', 'ijtebowcompte13');
+define('SERVEUR_BD', 'ijtebowcompte13.mysql.db');
+define('PORT_BD', '');
+define('LOGIN_BD', 'ijtebowcompte13');
+define('PASSE_BD', 'v8ng67SF2026');
 
 try {
-    $port = defined('PORT_BD') && PORT_BD !== '' ? ';port=' . PORT_BD : '';
-    $dsn = 'mysql:host=' . SERVEUR_BD . $port . ';dbname=' . NOM_BD . ';charset=utf8mb4';
+  //  $port = defined('PORT_BD') && PORT_BD !== '' ? ';port=' . PORT_BD : '';
+  //  $dsn = 'mysql:host=' . SERVEUR_BD . $port . ';dbname=' . NOM_BD . ';charset=utf8mb4';
+//
+  //  $conn = new PDO(
+    //    $dsn,
+      //  LOGIN_BD,
+        //PASSE_BD,
+//        [
+  //        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    //        PDO::ATTR_EMULATE_PREPARES => false,
+      //  ]
+   // );
+//} catch (PDOException $e) {
+  //  die("Erreur de connexion : " . $e->getMessage()); }
 
-    $conn = new PDO(
-        $dsn,
-        LOGIN_BD,
-        PASSE_BD,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ]
-    );
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
+try {
+    $pdo = new PDO('mysql:host=' . SERVEUR_BD . ';dbname=' . NOM_BD, LOGIN_BD, PASSE_BD);}
+
+} 
+catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage()); }
+
+
 ?>
