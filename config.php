1310fbf93bd3e222aec31a831a9e2ce18ec8206a
guillaume_Moulin <guillaumewm73@gmail.com>
2026-06-03 15:05:06 +0200

    commit

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
 
-define('NOM_BD', 'ijtebowcompte13');
-define('SERVEUR_BD', 'ijtebowcompte13.mysql.db');
-define('PORT_BD', '');
-define('LOGIN_BD', 'ijtebowcompte13');
-define('PASSE_BD', 'v8ng67SF2026');
+define('NOM_BD', 'sae203_bdd_v1');
+define('SERVEUR_BD', '127.0.0.1');
 
+define('LOGIN_BD', 'root');
+define('PASSE_BD', 'root');
