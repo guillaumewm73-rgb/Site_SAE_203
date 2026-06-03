3fdeb88f3565e866b5b27dc8ba5188a2644243d2
Olivia R <olivia.rattin07@gmail.com>
2026-06-03 15:03:37 +0200

    ajout config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('NOM_BD', 'ijtebowcompte13');
+define('SERVEUR_BD', 'ijtebowcompte13.mysql.db');
+define('PORT_BD', '');
+define('LOGIN_BD', 'ijtebowcompte13');
+define('PASSE_BD', 'v8ng67SF2026');
+
