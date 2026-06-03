7e075545b16bb23cd9895b900110f209049518b6
guillaume_Moulin <guillaumewm73@gmail.com>
2026-06-03 16:55:29 +0200

    Suppression mail SMTP

--- config.php
+++ config.php
@@ -5,11 +5,3 @@ define('SERVEUR_BD', 'localhost');
 
 define('LOGIN_BD', 'root');
 define('PASSE_BD', 'root');
-
-define('MAIL_ENABLED', true);
-define('MAIL_SMTP_HOST', 'smtp.gmail.com');
-define('MAIL_SMTP_USER', 'guillaumewm73@gmail.com');
-define('MAIL_SMTP_PASSWORD', 'evld azrl nbc xqzt');
-define('MAIL_SMTP_PORT', 587);
-define('MAIL_FROM_ADDRESS', 'guillaumewm73@gmail.com');
-define('MAIL_FROM_NAME', 'e-llusion');
